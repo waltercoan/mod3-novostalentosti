9403d3dd738943bc474a0fac42a9c3886a4eb9f6
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-08-08 14:33:35 -0300

    tipos de dados do javascript

---
+++ introjs/script2.js
@@ -0,0 +1,40 @@
+// VARIAVEL GLOBAL
+//INTEIRO
+//TIPO NUMERICO = NUMBER
+a = 10;
+console.log(a + " " + typeof(a));
+//STRING INTERPOLATION
+console.log(`a ${a} ${typeof(a)}`);
+//DECIMAL
+a = 20.5
+console.log(a + " " + typeof(a));
+//COMPARANDO SE VALORES SAO IGUAIS
+console.log(a == 20.5);
+console.log(a == "20.5");
+//COMPARANDO SE VALORES SAO IGUAIS
+// E OS TIPOS SÃO IGUAIS
+console.log(a === "20.5");
+//TIPO LÓGICO
+a = true; //false
+console.log(a + " " + typeof(a));
+console.log(a == "0");
+//TIPO TEXTO
+a = 'Porquê você não passa lá...';
+console.log(a + " " + typeof(a));
+a = "Porquê você não passa lá...";
+console.log(a + " " + typeof(a));
+//array - listas (object)
+a = ['um', 'dois', 'três'];
+console.log(a + " " + typeof(a));
+a = Array();
+a[0]="lalala";
+a.push("lelele");
+console.log(a.pop()); // STACK - PILHA
+console.log(a + " " + typeof(a));
+
+//OBJETO
+a = new Object();
+a.idade = 20;
+a.nome = "zezinho";
+console.log(a + " " + typeof(a));
+console.log(JSON.stringify(a));
