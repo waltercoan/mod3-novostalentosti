a2e665dd927f8f9f0c1e6c1bdbc75fbd5e841b1b
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-08-11 14:52:33 -0300

    exercalculadora

--- exerciciocalc/script.js
+++ exerciciocalc/script.js
@@ -2,3 +2,9 @@ function click_botao(valor){
     let txttela = document.getElementById("txttela");
     txttela.value += valor;
 }
+
+function click_igual(){
+    let txttela = document.getElementById("txttela");
+    let resultado = eval(txttela.value);
+    txttela.value = resultado;
+}
