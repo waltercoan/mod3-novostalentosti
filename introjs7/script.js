c38f9cd7ea218867016a1b543d50f683cdcf71c5
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-08-30 17:30:18 -0300

    event loop javascript

--- introjs7/script.js
+++ introjs7/script.js
@@ -1,14 +1,20 @@
 function sum (a,b){
     return new Promise(function(resolve,reject){
+        if(a<0 || b<0){
+            reject("Valores negativos não suportados");
+        }
         setTimeout(() => {
             resolve( a + b);
         }, 500);
     });
 }
-
-sum(2,2).then((result) => {
-    //console.log(result);
-    return result;
-}).then((result2)=>{
-    console.log(result2);
+const a = sum(-2,2).then((result) => {
+    console.log(result);
+    //return result;
+}).catch((err) =>{
+    console.log(err);
 });
+console.log(a);
+//.then((result2)=>{
+//    console.log(result2);
+//});
---
+++ introjs8/script.js
@@ -0,0 +1,17 @@
+function sum (a, b){
+    return new Promise((resolve, reject) =>{
+        if(a < 0 || b < 0){
+            reject("Valor inválido");
+        }
+        setTimeout(() => {
+            resolve(a + b);
+        }, 500);
+    })
+}
+(async function (){
+    const result = await sum(-2,2)
+        .catch((err)=>{
+            console.log(err);
+        });
+    console.log(result);
+})();
