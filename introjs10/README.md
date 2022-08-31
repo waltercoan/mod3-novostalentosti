8ef6aa4539f0dbe1bda2aefbfb11de0cd019a885
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-08-31 17:15:22 -0300

    js es6+

--- introjs10/README.md
+++ introjs10/README.md
@@ -47,3 +47,38 @@ console.log(soma(3,5)); //x = 3, y = 5
 let soma2 = (x=1 , y=2) => x + y
 console.log(soma2());
 ```
+
+## Rest Parameters
+Define uma coleção não limitada de valores por parâmetro.
+```javascript
+function minhafuncao(...valores){
+    console.log(valores);
+}
+minhafuncao();
+minhafuncao(1);
+minhafuncao(1,2);
+```
+
+## Desconstruir uma lista
+Retirar cada elemento da lista e atribuir a variáveis diferentes
+```javascript
+const lista =['🐶','🐊','🐸','🐧'];
+let a, b, c, d;
+[a,b,c,d] = lista; //desconstrutor
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+```
+
+## Espalhar valores (SPREAD Operator)
+Distribui os valores da lista para cada parâmetro de entrada
+```javascript
+function minhafunc(a,b,c,d){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);    
+}
+minhafunc(...lista); //SPREAD Operator
+```
--- introjs10/script.js
+++ introjs10/script.js
@@ -21,7 +21,7 @@ listavaloresunicos.add('🐶');
 listavaloresunicos.add('🐸');
 console.log(listavaloresunicos);*/
 
-function soma(x = 1, y = 2){
+/*function soma(x = 1, y = 2){
     return x + y;
 }
 console.log(soma()); //x = 1, y = 2
@@ -30,3 +30,48 @@ console.log(soma(3,5)); //x = 3, y = 5
 
 let soma2 = (x=1 , y=2) => x + y
 console.log(soma2());
+*/
+/*
+function outrafuncao(){
+    for(let umitem of arguments){
+        console.log(umitem);
+    }
+}
+outrafuncao(1);
+outrafuncao(11);
+
+
+function minhafuncao(...valores){
+    console.log(valores);
+}
+minhafuncao();
+minhafuncao(1);
+minhafuncao(1,2);
+
+console.log(1);
+console.log(1,2);
+console.log(1,2,3);
+
+function minhafuncao2(val1, val2=2, ...valores){
+    console.log("val1",val1);
+    console.log("val2",val2);
+    console.log(valores);
+}
+minhafuncao2(11,22,33,44);
+*/
+
+const lista =['🐶','🐊','🐸','🐧'];
+let a, b, c, d;
+[a,b,c,d] = lista;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+function minhafunc(a,b,c,d){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);    
+}
+minhafunc(...lista); //SPREAD Operator
