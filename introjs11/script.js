51d3990662fb2aa3efa697e7fa9eb22de01924d1
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-01 16:08:23 -0300

    fetch

--- introjs11/script.js
+++ introjs11/script.js
@@ -1,8 +1,11 @@
 (function (){
+    /*const result = 
+        await fetch("https://ws.apicep.com/cep/06233-030.json");
+    console.log(result);*/
+
     fetch("https://ws.apicep.com/cep/06233-030.json")
-    //.then((response) => console.log(response))
     .then(function(response){ //callback
         console.log(response);
     });
-
+    
 })();
