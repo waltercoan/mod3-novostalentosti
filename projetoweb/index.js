ddc664796b7475bb41ce321b0d1d67a7e8c5dfc0
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-08 17:33:03 -0300

    aplicacao web nodejs express

--- projetoweb/index.js
+++ projetoweb/index.js
@@ -6,7 +6,8 @@ app.set('view engine', 'handlebars');
 app.engine('handlebars', engine());
 
 app.get("/", function(req,res){
-    res.send(`<h1>EU NAO ACREDITO ${req.query['nome']} ${req.query['sobrenome']}</h1>`);
+    //res.send(`<h1>EU NAO ACREDITO ${req.query['nome']} ${req.query['sobrenome']}</h1>`);
+    res.render('index');
 })
 
 app.listen(3000, () =>{ //CALLBACK
