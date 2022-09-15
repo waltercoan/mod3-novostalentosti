8ed7d00be52a72dd2a9061ad11efcd02e72b4f5f
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-09-15 14:47:45 -0300

    wip: remover registro

--- projetoweb/public/site.js
+++ projetoweb/public/site.js
@@ -1,1 +1,9 @@
 /*ESSE CODIGO RODA NO NAVEGADO DO CLIENTE */
+(function(){
+    //let reftabela = document.getElementById("clientes");
+    $("#clientes").on("click",".js-delete", function(){
+        let botaoClicado = $(this);
+        alert(botaoClicado.attr("data-id"));
+
+    });
+})();
